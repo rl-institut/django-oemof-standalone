aa099f9369b18fb962cc7e68dc29a1cffb8f24e0
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2024-10-29 10:40:55 +0100

    Add example hooks

--- postprocess.py
+++ postprocess.py
@@ -5,10 +5,9 @@ OEMOF_PARAMETERS = {}
 
 if __name__ == "__main__":
     init_django()
-    from django_oemof import simulation, models
+    from django_oemof import models
 
-    simulation_id = simulation.simulate_scenario(scenario=OEMOF_DATAPACKAGE, parameters=OEMOF_PARAMETERS)
-    sim = models.Simulation.objects.get(id=simulation_id)
+    sim = models.Simulation.objects.get(id=1)
     inputs, outputs = sim.dataset.restore_results()
 
     # Do your stuff as usual
--- simulate.py
+++ simulate.py
@@ -1,11 +1,35 @@
 from setup import init_django
 
 OEMOF_DATAPACKAGE = "dispatch"
-OEMOF_PARAMETERS = {}
+OEMOF_PARAMETERS = {
+    "electricity_demand": 7000
+}
+
+
+def test_parameter_hook(scenario, parameters, request):
+    parameters["demand0"] = {"amount": parameters.pop("electricity_demand")}
+    return parameters
+
+
+def test_model_hook(scenario, model, request):
+    # Adapt model here
+    return model
+
+def test_es_hook(scenario, es, request):
+    return es
+
 
 if __name__ == "__main__":
     init_django()
-    from django_oemof import simulation
+    from django_oemof import simulation, hooks
+
+    ph = hooks.Hook(OEMOF_DATAPACKAGE, test_parameter_hook)
+    esh = hooks.Hook(OEMOF_DATAPACKAGE, test_es_hook)
+    mh = hooks.Hook(OEMOF_DATAPACKAGE, test_model_hook)
+
+    hooks.register_hook(hook_type=hooks.HookType.PARAMETER, hook=ph)
+    hooks.register_hook(hook_type=hooks.HookType.ENERGYSYSTEM, hook=esh)
+    hooks.register_hook(hook_type=hooks.HookType.MODEL, hook=mh)
 
     simulation_id = simulation.simulate_scenario(scenario=OEMOF_DATAPACKAGE, parameters=OEMOF_PARAMETERS)
     print("Simulation ID:", simulation_id)
